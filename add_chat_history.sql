de8e2003e60e3ebaff1b908e07788d04a0beba4a
Pham Quoc Dat <phamquocdat797979@gmail.com>
2026-05-18 18:20:39 +0700

    Clean up unnecessary files

--- add_chat_history.sql
+++
@@ -1,5 +0,0 @@
--- Chạy câu lệnh này trong SQL Editor của Supabase để thêm một thùng chứa siêu to (JSONB)
--- Thùng chứa này sẽ thay thế localStorage, lưu trữ toàn bộ các câu hỏi của BOT trên Đám mây.
-
-ALTER TABLE profiles 
-ADD COLUMN IF NOT EXISTS chat_history JSONB DEFAULT '[]'::jsonb;
